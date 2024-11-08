4c51730e8d4edcc15de5d09eb0b2b2854bb41ee8
marsya505 <marsyazulfazahrani0@gmail.com>
2024-11-08 12:00:30 +0700

    question 2

--- Square11.java
+++ Square11.java
@@ -4,8 +4,8 @@ public class Square11 {
         Scanner input11 = new Scanner(System.in);
         System.out.print("Enter value N = ");
         int N = input11.nextInt(); 
-        for (int iOuter=0; iOuter<=N; iOuter++) {
-            for (int i=1; i<=N; i++) {
+        for (int iOuter=1; iOuter<=N; iOuter++) {
+            for (int i=0; i<=N; i++) {
                 System.out.print("*");
             }
         System.out.println();
