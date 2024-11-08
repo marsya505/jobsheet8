11f64d10951ab54a0d1bdb2f559c7abf3c746dc8
marsya505 <marsyazulfazahrani0@gmail.com>
2024-11-08 12:03:26 +0700

    question 4

--- Square11.java
+++ Square11.java
@@ -5,10 +5,10 @@ public class Square11 {
         System.out.print("Enter value N = ");
         int N = input11.nextInt(); 
         for (int iOuter=1; iOuter<=N; iOuter++) {
-            for (int i=0; i<=N; i++) {
+            for (int i=1; i<=N; i++) {
                 System.out.print("*");
             }
-        System.out.println();
+        
         }
     }
 }
